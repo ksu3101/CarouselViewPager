299a192324a0fcafc4deba322adcfd73d0040640
ksu3101 <burkdog@naver.com>
2016-08-17 17:13:10 +0900

    [Update] update prototype sources.

--- app/src/main/java/kr/swkang/carouselviewpager/main/MainActivity.java
+++ app/src/main/java/kr/swkang/carouselviewpager/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ import kr.swkang.carouselviewpager.R;
 import kr.swkang.carouselviewpager.main.model.UserCardItem;
 import kr.swkang.carouselviewpager.main.sub.CardFragment;
 import kr.swkang.carouselviewpager.utils.BaseActivity;
+import kr.swkang.carouselviewpager.utils.ViewPagerTransformer;
 import kr.swkang.carouselviewpager.utils.mvp.BasePresenter;
 
 public class MainActivity
@@ -36,16 +38,41 @@ public class MainActivity
     setContentView(R.layout.main_activity);
 
     viewPager = (ViewPager) findViewById(R.id.main_ViewPager);
+    viewPager.setOffscreenPageLimit(3);
+    viewPager.setPageMargin(20);
+    viewPager.setClipToPadding(false);
+    viewPager.setPageTransformer(true, new ViewPagerTransformer());
+    viewPager.addOnPageChangeListener(
+        new ViewPager.OnPageChangeListener() {
+          @Override
+          public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+          }
+
+          @Override
+          public void onPageSelected(int position) {
+
+          }
+
+          @Override
+          public void onPageScrollStateChanged(int state) {
+          }
+        }
+    );
 
     ArrayList<UserCardItem> list = new ArrayList<>();
 
     adapter = new CarouselViewPagerAdapter(getSupportFragmentManager(), list);
+    viewPager.setAdapter(adapter);
+
+    // retrieve datas
+    presenter.retrieveUserList();
 
   }
 
   @Override
   public void onRetrieveUserListCompleted(@NonNull ArrayList<UserCardItem> resultList) {
     if (adapter != null) {
+      Log.d("// // // // ", "// retrive datas = " + resultList.size());
       adapter.updateDatas(resultList);
     }
   }
